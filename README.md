6635a15e152d840dac5c33516cd608b9a2adf76d
MJ <m.jarecki78@gmail.com>
2019-04-09 22:55:56 +0200

    refactoring, add java doc

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Collibra-backend websocket
+# Collibra socket server
+
+# server and log configuration
+/resources/application.properties
+
+# Build:
+mvn package
+
+# Run server:
+java -jar <package-name>.jar
+
+e.g.
+java -jar collibra-1.0.0.jar
 
--- src/test/java/com/mj/collibra/chat/ChatServiceImplTest.java
+++ src/test/java/com/mj/collibra/chat/ChatServiceImplTest.java
@@ -1,10 +1,14 @@
 package com.mj.collibra.chat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ChatServiceImplTest {
+
+    @Before
+    public void setUp(){}
 
     @Test
     public void startSessionResponse() {
